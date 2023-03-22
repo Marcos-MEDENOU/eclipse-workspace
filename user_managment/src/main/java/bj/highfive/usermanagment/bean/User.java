f7332d79a6d1f65f74ff2ed300b882bbb6764e91
marcos-medenou <marcosmedenou@gmail.com>
2023-03-22 19:29:50 +0100

    @updated

---
+++ user_managment/src/main/java/bj/highfive/usermanagment/bean/User.java
@@ -0,0 +1,36 @@
+package bj.highfive.usermanagment.bean;
+
+public class User {
+	private int id;
+	private String name,email,country;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+}
